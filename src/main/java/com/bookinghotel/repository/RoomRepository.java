94718b3983cf749c06336f46410874f6e51765ad
AnonymoussDev <anonymous69.dev@gmail.com>
2023-02-15 14:22:55 +0700

    [update] add api crud room

--- src/main/java/com/bookinghotel/repository/RoomRepository.java
+++ src/main/java/com/bookinghotel/repository/RoomRepository.java
@@ -14,12 +14,14 @@ import java.util.Optional;
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
-  @Query(value = "SELECT * FROM rooms r WHERE (:keyword is null or r.title LIKE CONCAT('%', :keyword, '%')) AND (:filter is null or r.type = :filter)",
-      countQuery = "SELECT COUNT(*) FROM rooms r WHERE (:keyword is null or r.title LIKE CONCAT('%', :keyword, '%')) AND (:filter is null or r.type = :filter)",
-      nativeQuery = true)
-  Page<Room> findAllByKey(@Param("keyword") String keyword, @Param("filter") String filter, Pageable pageable);
-
   @Query("SELECT r FROM Room r WHERE r.id = ?1 and r.deleteFlag = false")
   Optional<Room> findById(Long id);
+
+  @Query(value = "SELECT * FROM rooms r WHERE (:keyword is null or r.title LIKE CONCAT('%', :keyword, '%')) " +
+      "AND (:filter is null or r.type = :filter) AND r.delete_flag = 0",
+      countQuery = "SELECT COUNT(*) FROM rooms r WHERE (:keyword is null or r.title LIKE CONCAT('%', :keyword, '%')) " +
+          "AND (:filter is null or r.type = :filter) AND r.delete_flag = 0",
+      nativeQuery = true)
+  Page<Room> findAllByKey(@Param("keyword") String keyword, @Param("filter") String filter, Pageable pageable);
 
 }
